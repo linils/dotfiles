80e9db6c8aec2f58651f157e49878022bcfd4b8e
Linus Nilsson <lanivecontact@gmail.com>
2025-07-26 00:10:50 +0200

    vim binds

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,11 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+keymap("n", "D", "dd", opts)
+keymap({ "n", "v" }, "H", "^", opts)
+keymap({ "n", "v" }, "L", "$", opts)
+
 keymap("n", "gd", vim.lsp.buf.definition, opts)
 keymap("n", "<C-f>", vim.lsp.buf.format, opts)
 keymap("n", "<C-r>", vim.lsp.buf.rename, opts)
 keymap("n", "<leader>h", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
-keymap({ "n", "v" }, "H", "^", opts)
-keymap({ "n", "v" }, "L", "$", opts)
