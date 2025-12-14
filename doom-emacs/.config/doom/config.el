4c70b9baf5d5eec4340899aae132fbb5071d3036
Linus Nilsson <lanivecontact@gmail.com>
2025-12-14 01:57:20 +0100

    Update Emacs config

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -35,7 +35,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
                                         ; `load-theme' function. This is the default:
-(setq doom-theme 'doom-meltbus)
+(setq doom-theme 'doom-homage-black)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -82,3 +82,6 @@
 
 (after! eglot
   (setq eglot-ignored-server-capabilities '(:inlayHintProvider)))
+
+(after! projectile
+  (setq projectile-auto-discover nil))
--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -43,15 +43,15 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;smooth-scroll     ; So smooth you won't believe it's not butter
+       smooth-scroll     ; So smooth you won't believe it's not butter
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        ;;(vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
-       workspaces          ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       ;;workspaces          ; tab emulation, persistence & separate workspaces
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere)  ; come to the dark side, we have cookies
